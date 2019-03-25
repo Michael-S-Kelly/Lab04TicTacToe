13f5942c8a71a2cea18f1494301d18d8d69e3c2a
Michael Kelly <mike.s.kelly@icloud.com>
2019-03-24 17:33:38 -0700

    Added Starter Code

--- TicTacToe/TicTacToe/Program.cs
+++ TicTacToe/TicTacToe/Program.cs
@@ -7,6 +7,14 @@ namespace TicTacToe
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            PlayGame();
+        }
+
+        static void PlayGame()
+        {
+            // TODO: Setup your game here. Create an introduction. 
+            // Create your players, and instantiate your Game class. 
+            // output to the console the winner
         }
     }
 }
